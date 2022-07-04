5627bbe2ba3e4b21305ed9c3e8b5f0a2ec0d40d0
AhmetovMihail <ahmetovmofficial@gmail.com>
2022-07-04 21:52:37 +0300

    Ladders added

---
+++ platformer/Assets/Scripts/Ladder.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    [SerializeField] private float _climbingSpeed;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+        {
+            GravityTurn(player, 0);
+
+            if (Input.GetKey(KeyCode.W))
+                MoveOnLadder(player, _climbingSpeed);
+            else if (Input.GetKey(KeyCode.S))
+                MoveOnLadder(player, -_climbingSpeed);
+            else
+                MoveOnLadder(player, 0);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+            GravityTurn(player, 1);
+    }
+
+    private static void GravityTurn(Player player, int value)
+    {
+        player.gameObject.GetComponent<Rigidbody2D>().gravityScale = value;
+    }
+
+    private void MoveOnLadder(Player player, float climbingSpeed)
+    {
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, climbingSpeed);
+    }
+
+}
